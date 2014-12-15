1c31b2f84660682c06ce24236ee16c1fab43f0e2
Amit Taware <amit.taware@gmail.com>
2014-12-15 15:56:58 +0530

    Nothing

--- src/test/java/bmicalculator/test/stepdef/BMI_Calc_StepDef.java
+++ src/test/java/bmicalculator/test/stepdef/BMI_Calc_StepDef.java
@@ -17,22 +17,26 @@ public class BMI_Calc_StepDef {
 
 	@When("^I enter \"([^\"]*)\" as age$")
 	public void Given_I_enter_as_age(String arg1) throws Throwable {
+		System.out.println("Entering Age as "+arg1);
 		hp.enterAge(arg1);
 	}
 
 	@And("^I enter \"([^\"]*)\" as height$")
 	public void Given_I_enter_as_height(String arg1) throws Throwable {
+		System.out.println("Entering Hight as "+arg1);
 		hp.enterHight(arg1);
 	}
 
 	@Given("^I enter \"([^\"]*)\" as weight$")
 	public void And_I_enter_as_weight(String arg1) throws Throwable {
+		System.out.println("Entering Weight as "+arg1);
 		hp.enterWeight(arg1);
 	}
 
 	@Then("^I should see \"([^\"]*)\" as bmi and \"([^\"]*)\" as category$")
 	public void Then_I_should_see_as_bmi_and_as_category(String arg1,
 			String arg2) throws Throwable {
+		System.out.println("Verifying BMI as per input ");
 		assertTrue("Something Went wrong", hp.verifyBMI(arg1, arg2));
 	}
 
